90e0e2d0a6c2b9fd73d030488e4567b7ecc65df2
Howard Kennedy <hkennedyvi@gmail.com>
2020-04-21 16:21:15 -0700

    express, handlebars, bulma initialzied

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+var exphbs = require("express-handlebars");
+const path = require("path");
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 3000;
+
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
