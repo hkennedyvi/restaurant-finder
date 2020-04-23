18e237f6dd59b88351737f7dace512a707f0c005
Howard Kennedy <hkennedyvi@gmail.com>
2020-04-23 13:25:06 -0700

    tile layout edit

--- public/assets/js/finder.js
+++ public/assets/js/finder.js
@@ -1,14 +1,27 @@
-// $(document).ready(getCharacters);
+$(document).ready(getCharacters);
 
-// function getCharacters() {
-//   const queryURL = "https://superheroapi.com/api.php/";
-//   const key = "126917828959679";
-//   const searchTerm = "/search/wolverine"
+$("#comic-btn").click(function() {
+    console.log("hiii")
+})
 
-//   $.ajax({
-//     url: queryURL + key + searchTerm,
-//     method: "GET"
-//   }).then(function(response) {
-//       console.log(response);
-//   });
-// }
+function getCharacters() {
+  const queryURL = "https://superheroapi.com/api.php/";
+  const key = "126917828959679";
+  const searchTerm = "/search/dr manhattan";
+
+  $.ajax({
+    url: queryURL + key + searchTerm,
+    method: "GET"
+  }).then(function(response) {
+      console.log(response);
+      console.log(response.results[0].name);
+      console.log(response.results[0].biography['full-name'])
+      console.log(response.results[0].biography.aliases)
+      console.log(response.results[0].powerstats);
+      console.log(response.results[0].image.url);
+      comicDiv = $("#comic-test");
+      const image = $("<img>")
+      image.attr("src", response.results[0].image.url);
+      comicDiv.append(image);
+  });
+}
