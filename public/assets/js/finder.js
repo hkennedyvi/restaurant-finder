23e6d32795f9afcde95ccd7b403ecdad53392361
Howard Kennedy <hkennedyvi@gmail.com>
2020-04-24 18:53:10 -0700

    character attributes assigned tiles

--- public/assets/js/finder.js
+++ public/assets/js/finder.js
@@ -1,144 +1,94 @@
-// $(document).ready(getCharacters);
-
 $("#comic-btn").click(function () {
-    console.log("hiii")
-    getCharacters("ozymandias");
+  console.log("hiii")
+  getCharacters("ozymandias");
 })
 
 $(".char-dropdown").change(function () {
-    let selectedChar = $(this).children("option:selected").val();
-    getCharacters(selectedChar);
-  
-    let input = "HELLO"
-    const test = `<div class="tile is-ancestor">
-    <div class="tile is-vertical is-8">
-      <div class="tile">
-        <div class="tile is-parent is-vertical">
-          <article class="tile is-child box">
-            <p class="title">${input}</p>
-            <p class="subtitle">Alias 1</p>
-            <p class="subtitle">Alias 2</p>
-          </article>
-          <article class="tile is-child box">
-            <p class="title">WORK</p>
-            <p class="subtitle">Occupation</p>
-            <p class="subtitle">Base</p>
-          </article>
-        </div>
-        <div class="tile is-parent">
-          <article class="tile is-child box">
-            <p class="title">STRENGTHS</p>
-            <div class="content">
-              <ul type="1">
-                <li>${input}</li>
-                <li>${input}</li>
-                <li>${input}</li>
-              </ul>
-            </div>
-          </article>
-        </div>
-      </div>
-      <div class="tile is-parent">
-        <article class="tile is-child box">
-          <p class="title">Wide column</p>
-          <p class="subtitle">Aligned with the right column</p>
-          <div class="content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor
-              vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
-          </div>
-        </article>
-      </div>
-    </div>
-    <div class="tile is-parent">
-      <article class="tile is-child box">
-        {{!-- <div class="content"> --}}
-        <figure class="image">
-          <img src="https://www.superherodb.com/pictures2/portraits/10/100/884.jpg">
-        </figure>
-        {{!-- </div> --}}
-      </article>
-    </div>
-  </div>`;
-
-    $("#comic-test").append(test);
-
-    console.log(selectedChar);
-
+  let selectedChar = $(this).children("option:selected").val();
+  getCharacterInfo(selectedChar);
 })
 
-function getCharacters(character) {
-    const key = "126917828959679";
-    // const searchTerm = "dr manhattan";
-    const queryURL = `https://superheroapi.com/api.php/${key}/search/${character}`;
+function getCharacterInfo(character) {
+  const key = "126917828959679";
+  // const searchTerm = "dr manhattan";
+  const queryURL = `https://superheroapi.com/api.php/${key}/search/${character}`;
 
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-        let charName = response.results[0].name;
-        let charFullName = response.results[0].biography['full-name'];
-        let alias = response.results[0].biography.aliases[0];
-        let charPowers = response.results[0].powerstats;
-        let charImage = response.results[0].image.url;
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    let charName = response.results[0].name;
+    let charFullName = response.results[0].biography['full-name'];
+    let eyeColor = response.results[0].appearance['eye-color'];
+    let hairColor = response.results[0].appearance['hair-color'];
+    let alias = response.results[0].biography.aliases[0];
+    let charImage = response.results[0].image.url;
 
-        const test = `<div class="tile is-ancestor">
+    let intelligence = response.results[0].powerstats.intelligence;
+    let strength = response.results[0].powerstats.intelligence;
+    let speed = response.results[0].powerstats.strength;
+    let durability = response.results[0].powerstats.durability;
+    let power = response.results[0].powerstats.power;
+
+    const test = `<div class="character-section tile is-ancestor">
         <div class="tile is-vertical is-8">
           <div class="tile">
             <div class="tile is-parent is-vertical">
-              <article class="tile is-child box">
-                <p class="title">${charName}</p>
-                <p class="subtitle">Alias 1</p>
-                <p class="subtitle">Alias 2</p>
+              <article class="tile is-child has-background-black is-radiusless box">
+                <p class="title has-text-warning">${charName}</p>
+                <p class="has-text-warning">Full Name: ${charFullName}</p>
+                <p class="has-text-warning">AKA: ${alias} </p>
               </article>
-              <article class="tile is-child box">
-                <p class="title">WORK</p>
-                <p class="subtitle">Occupation</p>
-                <p class="subtitle">Base</p>
+              <article class="tile is-child has-background-warning is-radiusless is-shadowless box">
+                <p class="title has-text-black">APPEARANCE</p>
+                <p class="has-text-black">Hair: ${hairColor}</p>
+                <p class="has-text-black">Eyes: ${eyeColor}</p>
               </article>
             </div>
             <div class="tile is-parent">
-              <article class="tile is-child box">
-                <p class="title">STRENGTHS</p>
-                <div class="content">
+              <article class="strength-tile tile is-child has-background-warning is-radiusless is-shadowless box">
+                <p class="title has-text-black has-text-centered">STRENGTHS</p>
+                <div class="content has-text-black">
                   <ul type="1">
-                    <li>${alias}</li>
-                    <li>${charName}</li>
-                    <li>${charFullName}</li>
+                    <li>STRENGTH: ${strength}</li>
+                    <li>INTELLIGENCE: ${intelligence}</li>
+                    <li>SPEED: ${speed}</li>
+                    <li>DURABILITY: ${durability}</li>
+                    <li>POWER: ${power}</li>
                   </ul>
                 </div>
               </article>
             </div>
           </div>
           <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">Wide column</p>
-              <p class="subtitle">Aligned with the right column</p>
+            <article class="tile is-child has-background-black is-radiusless box">
+              <p class="title has-text-warning">Wide column</p>
+              <p class="subtitle has-text-warning">Aligned with the right column</p>
               <div class="content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor
+                <p class="has-text-warning">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros,
+                  eu pellentesque tortor
                   vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
               </div>
             </article>
           </div>
         </div>
         <div class="tile is-parent">
-          <article class="tile is-child box">
-            {{!-- <div class="content"> --}}
+          <article class="tile is-child has-background-warning is-radiusless is-shadowless box">
             <figure class="image">
-              <img src=${charImage}>
+              <img src="${charImage}">
             </figure>
-            {{!-- </div> --}}
           </article>
         </div>
       </div>`;
-    
-        $("#comic-test").append(test);
+
+    $("#comic-test").append(test);
 
 
-        //   comicDiv = $("#comic-test");
-        //   const image = $("<img>")
-        //   image.attr("src", response.results[0].image.url);
-        //   comicDiv.append(image);
-    });
+    //   comicDiv = $("#comic-test");
+    //   const image = $("<img>")
+    //   image.attr("src", response.results[0].image.url);
+    //   comicDiv.append(image);
+  });
 }
